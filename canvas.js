b09f3bd979f56fd945481abd376b3041aee1ce2d
Kitti Füredi <40038606+Capkit@users.noreply.github.com>
2019-03-19 23:37:28 +0100

    Updates canvas with new functions.

--- canvas.js
+++ canvas.js
@@ -1,98 +1,191 @@
+
+
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 
-var mouse = {
-	x: undefined,
-	y: undefined
-}
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
-var maxRadius = 50;
-//var minRadius = 2;
+addEventListener('resize', () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
 
-var colorArray = [
-	'#ff6600',
-	'#d6a7ad',
-	'#fc00d2',
-	'#00ffe9',
-	'#00ff15'
-]
-
-
-window.addEventListener('mousemove', function(event) {
-	mouse.x = event.x;
-	mouse.y = event.y;
+    init();
 })
 
+// Objects
+function Star(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.speed = {
+	x: randomIntFromRange(-10, 10),
+	y: 3,
+	}
+    this.friction = 0.7; //súrlódás
+    this.gravity = 1;
 
-function Circle (x, y, radius, dx, dy) {                                                     
-	this.x = x;
-	this.y = y;
-	this.radius = radius;
-	this.dx = dx;
-	this.dy = dy;
-	this.minRadius = radius;
-	this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+}
 
-this.draw = function() {
+Star.prototype.draw = function() {
+    c.save();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.shadowColor = 'white';
+    c.shadowBlur = 20;
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+    c.restore(); //csak erre a kódrészletre vonatkozik
+}
+
+Star.prototype.update = function() {
+    this.draw();
+
+    if (this.y + this.radius + this.speed.y > canvas.height) {
+    this.speed.y = -this.speed.y * this.friction;
+    this.shatter();	
+	} else {
+	this.speed.y += this.gravity;	
+	}
+
+    this.y += this.speed.y;
+    this.x += this.speed.x
+}
+
+Star.prototype.shatter = function () {
+     this.radius -= 3;
+     for (let i = 0; i < 8; i++) {
+	miniStars.push(new MiniStar(this.x, this.y, 2));		
+		}
+}
+
+//MINISTAR
+function MiniStar(x, y, radius, color) {
+    Star.call(this, x, y, radius, color);
+    this.speed = {
+	x: randomIntFromRange(-5, 5),
+	y: randomIntFromRange(-25, 25),
+	}
+    this.friction = 0.8; //súrlódás
+    this.gravity = 0.3;
+    this.ttl = 300;
+    this.opacity = 1;
+}
+
+
+MiniStar.prototype.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = `rgba(273, 254, 266, ${this.opacity})`;
+    c.shadowColor = 'white';
+    c.shadowBlur = 20;
+    c.fill();
+    c.closePath();
+}
+
+MiniStar.prototype.update = function() {
+    this.draw();
+
+    if (this.y + this.radius + this.speed.y > canvas.height) {
+    this.speed.y = -this.speed.y * this.friction;	
+	} else {
+	this.speed.y += this.gravity;	
+	}
+
+    this.y += this.speed.y;
+    this.x += this.speed.x;
+    this.ttl -= 1;
+    this.opacity -= 1 / this.ttl;
+}
+
+//creating hills method
+ function createMountain(mountainAmount, height, color ) {
+	for(let i = 0; i<mountainAmount; i++) {
+	const mountainWidth = canvas.height / mountainAmount;
 	c.beginPath();
-	c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-	c.strokeStyle = 'green';
-	c.fillStyle = this.color; //egész szám a Math.floor-ral
-	c.stroke(); 
+	c.moveTo(i * mountainWidth, canvas.height);
+	c.lineTo(i * mountainWidth + mountainWidth * 2, canvas.height);
+	c.lineTo(i * mountainWidth + mountainWidth, canvas.height - height);
+	c.lineTo(i * mountainWidth, canvas.height);
+	c.fillStyle = color;
 	c.fill();
-	}
-
-this.update = function() {	
-	if (this.x + this.radius > canvas.width|| this.x - this.radius < 0) {
-	this.dx = -this.dx;
-	} 
-
-
-	if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
-	this.dy = -this.dy;
-	} 
-
-	this.x += this.dx;
-	this.y += this.dy;
-
-	if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-		if(this.radius < maxRadius) {
-		this.radius += 1;
-		} 
-	} else if (this.radius > this.minRadius){
-	this.radius -= 1;
+	c.closePath();
 	}		
-	this.draw();
+}
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+// Implementation
+const backgroundGrad = c.createLinearGradient(0, 0, 0, canvas.height);
+backgroundGrad.addColorStop(0, '#1135ff');
+backgroundGrad.addColorStop(1, '#f4f6ff');
+
+let stars;
+let miniStars;
+//let backgroundStars;
+let ticker = 0;
+let fallingRate = 80;
+
+function init() {
+    stars = [];
+    miniStars = [];
+//    backgroundStars = [];
+
+    //for (let i = 0; i < 1; i++) {
+    //    stars.push(new Star(canvas.width / 2, 30, 30, '#fcfeff'));
+    //	}
+
+//    for (let i = 0; i < 100; i++) {
+//	const x = Math.random() * canvas.width;
+//	const y = Math.random() * canvas.height;
+//	const radius = Math.random() * 3;
+//	backgroundStars.push(new Star(x, y, radius, 'rgba(255, 255, 255, 0.8)'));
+//	}
+}
+
+// Animation Loop
+function animate() {
+    requestAnimationFrame(animate);
+    c.fillStyle = backgroundGrad;
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+//   backgroundStars.forEach(backgroundStar => {
+//	backgroundStar.draw();
+//	})
+
+ //   createMountain(1, 200, '#ffa35e');
+
+    stars.forEach((star, index) => {
+	star.update();
+	if (star.radius == 0) {
+	stars.splice(index, 1);
 	}
-}
+	});
 
+    miniStars.forEach((miniStar, index) => {
+	miniStar.update();
+	if (miniStar.ttl == 0) {
+	miniStars.splice(index, 1);	
+	}	
+	});
 
-var circleArray = [];
+    ticker++;
 
-for (var i =0; i <800; i++) {
-	var radius = Math.random() * 3 + 1;
-	var x = Math.random() * (innerWidth - radius * 2) + radius;
-	var y = Math.random() * (innerHeight - radius * 2) + radius;
-	var dx = (Math.random() - 0,5) /4;
-	var dy = (Math.random() - 0,5) /4;
-	circleArray.push(new Circle(x, y, radius, dx, dy)) + radius;
-}
-
-function animatingCircle () {
-
-	requestAnimationFrame(animatingCircle);
-	c.clearRect(0, 0, innerWidth, innerHeight);
-
-for (var i = 0; i < circleArray.length; i++){
-	circleArray[i].update();
-}
+    if(ticker % fallingRate == 0) {
+	const x = Math.random() * canvas.width;
+	stars.push(new Star(x, -100, 12, 'white'));
+	fallingRate = randomIntFromRange(80, 200);	
+	}
 	
-
 }
 
-animatingCircle();
+init();
+animate();
 
